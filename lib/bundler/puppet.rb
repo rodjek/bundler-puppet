eb2006f1e1794b774f07c8056f3148ee88a94437
Tim Sharpe <tim@sharpe.id.au>
2012-05-15 11:40:14 +1000

    Add hacks from @aussielunix's fork

--- lib/bundler/puppet.rb
+++ lib/bundler/puppet.rb
@@ -4,8 +4,7 @@ module Bundler
   module Puppet
     module DslPatch
       def git_puppet(uri, options = {}, source_options = {}, &blk)
-        puts "Woot"
-        source Source::Git.new(_normalize_hash(options).merge("uri" => uri)), source_options, &blk
+        source ::Bundler::Puppet::Source::Git.new(_normalize_hash(options).merge("uri" => uri)), source_options, &blk
       end
 
       def self.included(mod)
@@ -13,7 +12,65 @@ module Bundler
         mod.send :alias_method, :git, :git_puppet
       end
     end
+
+    module SettingsPatch
+      def path_puppet
+        key = key_for(:path)
+        path = ENV[key] || @global_config[key]
+        return path if path && !@local_config.key?(key)
+
+        if path = self[:path]
+          path
+        else
+          'modules'
+        end
+      end
+
+      def self.included(mod)
+        mod.send :alias_method, :path_non_puppet, :path
+        mod.send :alias_method, :path, :path_puppet
+      end
+    end
+
+    module Source
+      class Git < ::Bundler::Source::Git
+        def install_path
+          begin
+            git_scope = find_modulename(base_name)
+
+            if Bundler.requires_sudo?
+              Bundler.user_bundle_path.join(Bundler.ruby_scope).join(git_scope)
+            else
+              Bundler.install_path.join(git_scope)
+            end
+          end
+        end
+
+        alias :path :install_path
+      private
+        def find_modulename(str)
+          if str =~ /-puppet$/
+            part = 1
+          else
+            part = 2
+          end
+
+          str.split(/\A([^-\/|.]+)[-|\/](.+)\z/)[part]
+        end
+      end
+    end
   end
 
   Bundler::Dsl.send(:include, Bundler::Puppet::DslPatch)
+  Bundler::Settings.send(:include, Bundler::Puppet::SettingsPatch)
+
+  class << self
+    def home
+      bundle_path
+    end
+
+    def install_path
+      home
+    end
+  end
 end
